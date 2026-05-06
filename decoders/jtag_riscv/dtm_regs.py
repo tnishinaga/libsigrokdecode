288c52788fa49283690e496f64e0981fa6b58b30
Toshifumi NISHINAGA <tnishinaga.dev@gmail.com>
2026-05-06 16:18:49 +0900

    wip

--- decoders/jtag_riscv/dtm_regs.py
+++ decoders/jtag_riscv/dtm_regs.py
@@ -95,6 +95,14 @@ class Dmi:
                     data = str(DmStatus(self.data))
                 case DmRegAddress.dmcontrol:
                     data = str(DmControl(self.data))
+                case DmRegAddress.hartinfo:
+                    data = str(HartInfo(self.data))
+                case DmRegAddress.abstractcs:
+                    data = str(AbstractControlAndStatus(self.data))
+                case DmRegAddress.command:
+                    data = str(AbstractCommand(self.data))
+                case DmRegAddress.abstractauto:
+                    data = str(AbstractCommandAutoExec(self.data))
                 case _:
                     data = f"{self.data:#010x}"
                     pass
@@ -272,5 +280,229 @@ class HartInfo:
         buf += "data_access: {}({})".format("CSR" if self.data_access else {"mmap"}, self.data_access)
         buf += f"data_size: {self.data_size}, "
         buf += f"data_addr: {self.data_addr}, "
+        buf += f"}}({self.data:#010x})"
+        return buf
+    
+class AbstractCmdErr(IntEnum):
+    No = 0
+    Busy = 1
+    NotSupported = 2
+    Exception = 3
+    HaltResume = 4
+    Bus = 5
+    Reserved = 6
+    Other = 7
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class AbstractControlAndStatus:
+    progbufsize: int = 0
+    cmderr: AbstractCmdErr = AbstractCmdErr.No
+    datacount: int = 0
+    data: int = 0
+
+    def __init__(self, data: int):
+        self.progbufsize = (data >> 24) & 0b11111 # 5bits
+        self.busy = True if (data >> 12) & 1 == 1 else { False}
+        self.cmderr = AbstractCmdErr((data >> 8) & 0b111)  # 3bits
+        self.datacount = (data >> 0) & 0b1111  # 4bits
+        self.data = data
+
+    def __str__(self) -> str:
+        buf = f"AbstractControlAndStatus({DmRegAddress.abstractcs:#04x}) {{"
+        buf += f"progbufsize: {self.progbufsize}, "
+        buf += f"cmderr: {self.cmderr}({self.cmderr.value}), "
+        buf += f"datacount: {self.datacount} "
+        buf += f"}}({self.data:#010x})"
+        return buf
+    
+class AbstractCommandType(IntEnum):
+    Register = 0
+    Quick = 1
+    Memory = 2
+    def __str__(self) -> str:
+        return self.name
+
+class AbstractAccessRegister:
+    simple_flags: dict[str, int] = {}
+    aarsize: int = 0
+    regno: int = 0
+    data: int = 0
+
+    def __init__(self, data: int):
+        self.aarsize = (data >> 20) & 0b111 # 3bits
+        self.simple_flags["aarpostincrement"] = (data >> 19) & 1
+        self.simple_flags["postexec"] = (data >> 18) & 1
+        self.simple_flags["transfer"] = (data >> 17) & 1
+        self.simple_flags["write"] = (data >> 16) & 1
+        self.regno = (data >> 0) & 0xffff # 16bits
+        self.data = data
+
+    def __str__(self) -> str:
+        buf = f"AccessRegister({AbstractCommandType.Register.value}) {{"
+        buf += f"aarsize: {self.aarsize}, "
+        for field in filter(lambda x: x[1] == 1, self.simple_flags.items()):
+            buf += f"{field[0]}(1), "
+        buf += f"regno: {self.regno:#06x}"
+        buf += "}"
+        return buf
+    
+class AbstractAccessMemory:
+    virtual: int = 0
+    size: int = 0
+    post_increment: int = 0
+    write: int = 0
+    target_specific: int = 0
+
+    def __init__(self, data: int):
+        self.virtual = (data >> 23) & 1
+        self.size = (data >> 20) & 0b111 # 3bits
+        self.post_increment = (data >> 19) & 1
+        self.write = (data >> 16) & 1
+        self.target_specific = (data >> 14) & 0b11 # 2bits
+        self.data = data
+
+    def __str__(self) -> str:
+        buf = f"AccessMemory({AbstractCommandType.Memory.value}) {{"
+        buf += f"virtual({self.virtual}), "
+        match self.size:
+            case 0:
+                buf += f"size: 8-bits({self.size}), "
+            case 1:
+                buf += f"size: 16-bits({self.size}), "
+            case 2:
+                buf += f"size: 32-bits({self.size}), "
+            case 3:
+                buf += f"size: 64-bits({self.size}), "
+            case 4:
+                buf += f"size: 128-bits({self.size}), "
+            case _:
+                buf += f"size: Unknown({self.size}), "
+        buf += f"post_increment({self.post_increment}), "
+        buf += f"write({self.write}), "
+        buf += f"target-specific: {self.target_specific}"
+        buf += "}"
+        return buf
+
+class AbstractCommand:
+    cmd_type:int = 0
+    control: int = 0
+
+    def __init__(self, data: int):
+        self.cmd_type = (data >> 24) & 0xff # 8bits
+        self.control = (data >> 0) & 0x00FF_FFFF  # 24bits
+        self.data = data
+
+    def __str__(self) -> str:
+        buf = f"AbstractCommand({DmRegAddress.command:#04x}) {{"
+        try:
+            cmd_type = AbstractCommandType(self.cmd_type)
+        except ValueError:
+            buf += f"cmd_type: {self.cmd_type}, "
+            buf += f"control: {self.control:#08x}"
+        match cmd_type:
+            case AbstractCommandType.Register:
+                buf += str(AbstractAccessRegister(self.control))
+            case AbstractCommandType.Quick:
+                buf += f"cmd_type: {str(cmd_type)}({self.cmd_type}), "
+            case AbstractCommandType.Memory:
+                buf += str(AbstractAccessMemory(self.control))
+            case _:
+                buf += f"cmd_type: {str(cmd_type)}({self.cmd_type}), "
+                buf += f"control: {self.control:#08x}"
+        buf += f"}}({self.data:#010x})"
+        return buf
+
+class AbstractCommandAutoExec:
+    autoexec_progbuf:int = 0
+    autoexec_data: int = 0
+
+    def __init__(self, data: int):
+        self.autoexec_progbuf = (data >> 16) & 0xffff # 16bits
+        self.autoexec_data = (data >> 0) & 0x0FFF  # 12bits
+        self.data = data
+
+    def __str__(self) -> str:
+        buf = f"AbstractCommandAutoExec({DmRegAddress.abstractauto:#04x}) {{"
+        buf += f"autoexec_progbuf: {self.autoexec_progbuf:#06x}, "
+        buf += f"autoexec_data: {self.autoexec_data:#05x}"
+        buf += f"}}({self.data:#010x})"
+        return buf
+
+
+class SbAccess(IntEnum):
+    Bit8 = 0
+    Bit16 = 1
+    Bit32 = 2
+    Bit64 = 3
+    Bit128 = 4
+
+    def __str__(self) -> str:
+        match self:
+            case SbAccess.Bit8:
+                "8bit"
+            case SbAccess.Bit16:
+                "16bit"
+            case SbAccess.Bit32:
+                "32bit"
+            case SbAccess.Bit64:
+                "64bit"
+            case SbAccess.Bit128:
+                "128bit"
+
+class SbError(IntEnum):
+    NoError = 0
+    Timeout = 1
+    BadAddress = 2
+    Alignment = 3
+    UnsupportedSize = 4
+    Other = 7
+
+    def __str__(self) -> str:
+        return self.name
+    
+# sbcs
+class SystemBusAccessControlAndStatus:
+    sb_version:int = 0
+    sb_access:int = 0
+    sb_error:int = 0
+    sb_address_size:int = 0
+    simple_flags: dict[str, int] = {}
+
+    def __init__(self, data: int):
+        self.sb_version = (data >> 29) & 0b111 # 3bits
+        self.simple_flags["sbbusyerror"] = (data >> 22) & 1
+        self.simple_flags["sbbusy"] = (data >> 21) & 1
+        self.simple_flags["sbreadonaddr"] = (data >> 20) & 1
+        self.sb_access = (data >> 17) & 0b111 # 3bits
+        self.simple_flags["sbautoincrement"] = (data >> 16) & 1
+        self.simple_flags["sbreadondata"] = (data >> 15) & 1
+        self.sb_error = (data >> 12) & 0b111 # 3bits
+        self.sb_address_size = (data >> 5) & 0b1111111 # 7bits
+        self.simple_flags["sbaccess128"] = (data >> 4) & 1
+        self.simple_flags["sbaccess64"] = (data >> 3) & 1
+        self.simple_flags["sbaccess32"] = (data >> 2) & 1
+        self.simple_flags["sbaccess16"] = (data >> 1) & 1
+        self.simple_flags["sbaccess8"] = (data >> 0) & 1
+        self.data = data
+
+    def __str__(self) -> str:
+        buf = f"SystemBusAccessControlAndStatus({DmRegAddress.sbcs:#04x}) {{"
+        buf += f"version: {self.sb_version}, "
+        for field in filter(lambda x: x[1] == 1, self.simple_flags.items()):
+            buf += f"{field[0]}(1), "
+        try:
+            access = str(SbAccess(self.sb_access))
+            buf += f"access: {access}({self.sb_access}), "
+        except ValueError:
+            buf += f"access: Unknown({self.sb_access}), "
+        try:
+            error = str(SbError(self.sb_error))
+            buf += f"error: {error}({self.sb_error}), "
+        except ValueError:
+            buf += f"error: Unknown({self.sb_error}), "
+        buf += f"address_size: {self.sb_address_size:#05x}, "
         buf += f"}}({self.data:#010x})"
         return buf
